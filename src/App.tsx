8516b33850376d12784ab82a186f917e015234a2
Adam Schachne <adamschachne@live.com>
2019-10-30 19:17:34 -0700

    convert app to hook function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-bitwise */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -21,75 +21,73 @@ function color(red: number, green: number) {
   return 0b001100 + Math.min(Math.max(0, red), 3) + Math.min(Math.max(0, green), 3) * 8;
 }
 
-function onMidiAccessSuccess(access: WebMidi.MIDIAccess) {
-  const { inputs } = access;
-  const { outputs } = access;
-  const output: WebMidi.MIDIOutput = outputs.values().next().value;
-  const input: WebMidi.MIDIInput = inputs.values().next().value;
-
-  if (!input || !output) return [];
-  input.onmidimessage = createHandleMidiMessage(output);
-  return [input, output];
-}
-
-function onMidiAccessFailure(error: Error) {
-  console.log("Oops. Something were wrong with requestMIDIAccess", error);
-}
-
-function createHandleMidiMessage(output: WebMidi.MIDIOutput) {
-  return function handleMidiMessage(e: WebMidi.MIDIMessageEvent) {
-    // if (e.data[0] !== 0x90) return;
-    console.log(e.data);
-
-    // display data
-    // var data = document.createElement("pre");
-    // data.innerText = e.data;
-    // document.querySelector("#output").append(data);
-    // data.scrollIntoView();
-
-    const buttonId = e.data[1];
-    const release = e.data[2] === 0;
-    if (!release) {
-      return;
-    }
-
-    const red = ~~(Math.random() * 4);
-    const green = ~~(Math.random() * 4);
-    try {
-      if (buttonId === 104) {
-        for (let x = 0; x < 8; x += 1) {
-          for (let y = 0; y < 8; x += 1) {
-            output.send([144, xy2i(x, y), color(0, 0)]);
-          }
-        }
-      } else {
-        output.send([144, buttonId, color(red, green)]);
-      }
-    } catch {
-      //
-    }
-  };
-}
-
-interface State {
+interface MidiDevice {
   output: WebMidi.MIDIOutput;
   input: WebMidi.MIDIInput;
 }
 
-class App extends React.Component<any, State> {
-  componentDidMount() {
-    window.navigator.requestMIDIAccess().then(onMidiAccessSuccess, onMidiAccessFailure);
+function handleMidiMessage(output: WebMidi.MIDIOutput, e: WebMidi.MIDIMessageEvent) {
+  if (!output) {
+    return;
+  }
+  // if (e.data[0] !== 0x90) return;
+  const buttonId = e.data[1];
+  // console.log(e.data);
+  const press = e.data[2] === 127;
+  // const release = e.data[2] === 0;
+
+  if (!press) {
+    return;
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
-    );
+  const red = ~~(Math.random() * 4);
+  const green = ~~(Math.random() * 4);
+  try {
+    if (buttonId === 104) {
+      for (let x = 0; x < 8; x += 1) {
+        for (let y = 0; y < 8; x += 1) {
+          output.send([144, xy2i(x, y), color(0, 0)]);
+        }
+      }
+    } else {
+      output.send([144, buttonId, color(red, green)]);
+    }
+  } catch {
+    //
   }
+}
+
+function App() {
+  const [midi, setMidi] = useState<MidiDevice | null>(null);
+  useEffect(() => {
+    if (!midi) {
+      window.navigator.requestMIDIAccess().then(
+        ({ inputs, outputs }) => {
+          const midiOut: WebMidi.MIDIOutput = outputs.values().next().value;
+          const midiIn: WebMidi.MIDIInput = inputs.values().next().value;
+          if (!midiIn || !midiOut) return;
+          midiIn.onmidimessage = (e) => handleMidiMessage(midiOut, e);
+          setMidi({ input: midiIn, output: midiOut });
+        },
+        (err) => {
+          console.log("Oops. Something were wrong with requestMIDIAccess", err);
+        }
+      );
+    }
+    return () => {
+      if (midi) {
+        midi.input.close();
+        midi.output.close();
+      }
+    };
+  }, [midi]);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
 }
 
 export default App;
